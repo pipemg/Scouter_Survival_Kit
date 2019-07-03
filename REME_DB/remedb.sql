1275921960c81d55c36a872abadc8bbfd461e344
travisbot <support@travis-ci.org>
2019-07-02 20:41:25 -0500

    update sql

--- REME_DB/remedb.sql
+++ REME_DB/remedb.sql
@@ -7,7 +7,6 @@ CREATE TABLE reme_main (
 );
 
 
-
 CREATE TABLE reme_synonyms (
 	reme_id INT(6) UNSIGNED ZEROFILL,
 	name VARCHAR(50) NOT NULL, 
@@ -33,6 +32,37 @@ CREATE TABLE reme_sections (
 	age_max INT DEFAULT 99,
 	PRIMARY KEY (id)
 );
+
+
+CREATE TABLE reme_objetivos_educativos (
+	oe_id INT AUTO_INCREMENT,
+	seccion ENUM('Manada','Tropa','Comunidad','Clan')  NOT NULL,
+	area_crecimiento ENUM() NOT NULL,
+	tipo	ENUM('Presa','Dentellada','Territorio','Exploración','Desafio','Camino','Objetivo')  NOT NULL,
+	definicion VARCHAR(100) NOT NULL,
+	PRIMARY KEY(oe_id)
+);
+
+
+
+CREATE TABLE reme_objetivos_terminales (
+	id INT AUTO_INCREMENT,
+	reme_id INT(6) UNSIGNED ZEROFILL,
+	seccion ENUM('Manada','Tropa','Comunidad','Clan')  NOT NULL,
+	presas_im VARCHAR(200),
+	presas_it VARCHAR(200),
+	dentelladas VARCHAR(200),
+	territorios_pre VARCHAR(200),
+	territorios_pub VARCHAR(200),
+	exploraciones VARCHAR(200),
+	desafios VARCHAR(200),
+	caminos VARCHAR(200),
+	objetivos_terminales VARCHAR(200),
+	PRIMARY KEY(id)
+);
+
+
+
 
 
 CREATE TABLE reme_comments (
